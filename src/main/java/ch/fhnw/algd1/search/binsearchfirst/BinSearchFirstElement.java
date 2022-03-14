a08239ad86f1acc7e8ed70385f1936c3438cfedd
JoelBaumann1 <100359477+JoelBaumann1@users.noreply.github.com>
2022-03-14 13:27:09 +0100

    Initial commit

---
+++ src/main/java/ch/fhnw/algd1/search/binsearchfirst/BinSearchFirstElement.java
@@ -0,0 +1,8 @@
+package ch.fhnw.algd1.search.binsearchfirst;
+
+public class BinSearchFirstElement {
+	public static <T extends Comparable<? super T>> int binSearch(T[] data, T value) {
+		// TODO (A5) Implement bianry search for first element
+		return 0;
+	}
+}
---
+++ src/main/java/ch/fhnw/algd1/search/sentinelsearch/SentinelSearch.java
@@ -0,0 +1,13 @@
+package ch.fhnw.algd1.search.sentinelsearch;
+
+public class SentinelSearch {
+	public static boolean exists(int[] data, int value) {
+		// TODO (A2) Implement sequential search with sentinel
+		return false;
+	}
+
+	public static int firstIndex(int[] data, int value) {
+		// TODO (A2 extra) Find first occurrence on an element in data (advanced challenge)
+		return -1;
+	}
+}
